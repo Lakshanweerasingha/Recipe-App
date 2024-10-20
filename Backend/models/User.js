a2b34b7b1d00f275af30e2e2a05af841fe134e36
lakshanweerasingha <lakshanmaduwantha03@gmail.com>
2024-10-21 00:40:27 +0530

    ch

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true},
   favoriteRecipes: [{ type: String }],  // Array of recipe IDs
 });
 
