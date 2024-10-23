e140d1fab1629c731226e2f3c531676cbdd3c7cb
lakshanweerasingha <lakshanmaduwantha03@gmail.com>
2024-10-23 19:35:03 +0530

    change register

--- Frontend/src/Config/Api.jsx
+++ Frontend/src/Config/Api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'recipe-app-beige-eta.vercel.app/api';
 
 export const ENDPOINTS = {
   userProfile: `${API_BASE_URL}/users/profile`,
