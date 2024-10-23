899a828304ed58d8c3cf5f46c4b8b978205ae8b5
lakshanweerasingha <lakshanmaduwantha03@gmail.com>
2024-10-23 19:55:30 +0530

    reg

--- Frontend/src/Config/Api.jsx
+++ Frontend/src/Config/Api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://recipe-app-beige-eta.vercel.app';
+const API_BASE_URL = 'https://recipe-app-beige-eta.vercel.app/api';
 
 export const ENDPOINTS = {
   userProfile: `${API_BASE_URL}/users/profile`,
@@ -7,6 +7,6 @@ export const ENDPOINTS = {
   recipesByCategory: (category) => `${API_BASE_URL}/recipes/${category}`,
   deleteFavorite: (idMeal) => `${API_BASE_URL}/users/favorites/${idMeal}`,
   login: `${API_BASE_URL}/users/login`, 
-  register: `${API_BASE_URL}register`,  
+  register: `${API_BASE_URL}/users/register`,  
   mealCategories: `${API_BASE_URL}/recipes/categories`,  
 };
